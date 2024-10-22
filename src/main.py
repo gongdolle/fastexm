25720a80a7a5ec4a3020530ca8fb72951450c467
gongdolle <tyj456789@naver.com>
2024-10-22 09:52:59 +0900

    cors add

--- src/main.py
+++ src/main.py
@@ -2,6 +2,13 @@ from fastapi import FastAPI
 from src.api import todo,user
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://p-gridon.com"],  # React 프론트엔드 도메인
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(todo.router)
 app.include_router(user.router)
 
